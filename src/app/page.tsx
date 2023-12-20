e01fff28f12a91eb59bedce45b9800b285bb6012
Lucas Casal <lucasocteixeira@gmail.com>
2023-12-20 09:51:58 -0300

    ajuste final version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import BackgroundPicture2 from '../../public/foto-lucas-e-amanda-2.jpeg'
 import BackgroundPicture1 from '../../public/foto-amanda-e-lucas-3.jpeg'
 import { FaArrowRight } from "react-icons/fa";
 import '@fontsource/aboreto';
 import '@fontsource/gwendolyn';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { ModalContext, TypesOfModal } from '@/contexts/ModalContext';
 import CreatePostModal from '@/components/CreatePostModal/CreatePostModal';
 import axios from 'axios';
@@ -12,8 +11,6 @@ import axios from 'axios';
 
 export default function Home() {
   const { postModalOpen, setPostModalOpen, setTypeOfModal } = ModalContext()
-  const [bgImage, setBGImage] = useState<string>()
-  const [allGuestsArray, setAllGuestsArray] = useState<string[]>([])
 
   const handleModalOpen = (typeOfModal: TypesOfModal): void => {
     setTypeOfModal(typeOfModal)
@@ -24,7 +21,6 @@ export default function Home() {
     try {
       const response = await axios.get(`${process.env.NEXT_PUBLIC_DATABASE_URL}/guests`)
       localStorage.setItem('GL', JSON.stringify(response.data))
-      setAllGuestsArray(response.data)
       return response
     } catch (error) {
       console.log(error)
@@ -32,39 +28,35 @@ export default function Home() {
   }
   useEffect(() => {
     getNames()
-    defineBGImage()
   }, [])
 
-  const defineBGImage = () => {
-     return setBGImage(BackgroundPicture1.src)
-  }
 
   return (
     <>
       <main className="flex box-border h-screen flex-col items-center justify-between p-24 overflow-hidden">
         {postModalOpen ? <CreatePostModal /> : ''}
         <div className='fixed top-0 z-0 bg-black bg-opacity-40 overflow-hidden w-screen h-screen ' />
-        <img alt='background-image' src={BackgroundPicture1.src} className={(postModalOpen ? 'blur-md' : '') + ' fixed sm:-top-72 -top-20 -z-10 max-w-none sm:w-bgphoto h-full sm:h-auto sm:-left-3/4 1245:left-0 1245:w-screen 1000:-left-64 md:-left-96 1245:-top-32 lg:-top-72 10xl:-top-3/4 '} />
-        <h1 className={(postModalOpen ? 'hidden ' : '') + 'fixed top-0 left-0 sm:top-10 text-center sm:w-full font-gwendolyn font-thin z-0 text-white text-8xl'}>
+        <img alt='background-image' src={BackgroundPicture1.src} className={(postModalOpen ? 'blur-md' : '') + ' fixed sm:-top-72 -top-10 -z-10 max-w-none sm:w-bgphoto h-full sm:h-auto sm:-left-3/4 1245:left-0 1245:w-screen 1000:-left-64 md:-left-96 1245:-top-32 lg:-top-72 10xl:-top-3/4 '} />
+        <h1 className={(postModalOpen ? 'hidden ' : '') + 'fixed top-0 left-0 sm:top-10 text-center sm:w-full font-gwendolyn font-medium z-0 text-white text-6xl sm:text-8xl'}>
           Amanda e Lucas
         </h1>
 
-        <div className="absolute bottom-0 sm:bottom-32 h-auto w-screen grid gap-5 text-center sm:gap-28 lg:max-w-7xl sm:w-11/12 lg:mb-0 sm:h-28 sm:grid-cols-2 lg:text-left">
+        <div className="absolute bottom-0 sm:bottom-32 h-auto w-screen grid text-center gap-4 sm:gap-28 lg:max-w-7xl sm:w-11/12 lg:mb-0 sm:h-28 sm:grid-cols-2 lg:text-left">
 
 
           <div className='flex flex-col items-center justify-center'>
 
             <div
-              className={(postModalOpen ? 'hidden ' : '') + "group rounded-lg border bg-opacity-50 border-cyan-300 bg-gradient-to-b text-white h-11/12 1000:h-28 to-emerald-800 from-emerald-500 w-full sm:bg-transparent sm:bg-none sm:border-transparent px-5 py-2 transition-colors hover:cursor-pointer hover:bg-opacity-50 hover:border-cyan-300 hover:bg-gradient-to-b  hover:text-white hover:shadow-cyan-500 hover:shadow-xl hover:to-blue-800 hover:from-green-300"}
+              className={(postModalOpen ? 'hidden ' : '') + "group sm:rounded-lg bg-opacity-50 bg-gradient-to-b text-white h-11/12 1000:h-28 to-emerald-800 from-emerald-500 w-full sm:bg-transparent sm:bg-none border-y-2 border-emerald-900 sm:border-transparent px-5 py-2 transition-colors hover:cursor-pointer hover:bg-opacity-50 hover:border-cyan-300 hover:bg-gradient-to-b  hover:text-white hover:shadow-cyan-500 hover:shadow-xl hover:to-blue-800 hover:from-green-300"}
               onClick={() => handleModalOpen(TypesOfModal.CONFIRMATION)}
             >
-              <h2 className={`flex flex-row items-center justify-center w-full gap-1 mb-3 text-2xl font-semibold`}>
+              <h2 className={`flex flex-row items-center justify-center w-full gap-1 mb-3 text-2xl font-redressed`}>
                 Confirmar presença
-                <span className="flex items-center transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                <span className="hidden sm:flex items-center transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                   <FaArrowRight />
                 </span>
               </h2>
-              <p className={`m-0 w-full text-center text-sm tracking-wide`}>
+              <p className={`hidden sm:flex justify-center m-0 w-full text-center text-sm tracking-wide`}>
                 Confirme a sua presença na nossa festa!
               </p>
             </div>
@@ -73,16 +65,16 @@ export default function Home() {
           <div className='flex flex-col items-center justify-center'>
 
             <div
-              className={(postModalOpen ? 'hidden ' : '') + "group rounded-lg border bg-opacity-50 border-cyan-300 bg-gradient-to-b h-10/12 1000:h-28 text-white to-emerald-800 from-emerald-500 w-full sm:bg-transparent sm:bg-none sm:border-transparent px-5 py-2 transition-colors hover:cursor-pointer hover:bg-opacity-50 hover:border-cyan-300 hover:bg-gradient-to-b  hover:text-white hover:shadow-cyan-500 hover:shadow-xl hover:to-blue-800 hover:from-green-300"}
+              className={(postModalOpen ? 'hidden ' : '') + "group sm:rounded-lg  bg-opacity-50 bg-gradient-to-b h-14 1000:h-28 text-white to-emerald-800 from-emerald-500 w-full sm:bg-transparent sm:bg-none border-y-2 border-emerald-900 sm:border-transparent px-5 py-2 transition-colors hover:cursor-pointer hover:bg-opacity-50 hover:border-cyan-300 hover:bg-gradient-to-b  hover:text-white hover:shadow-cyan-500 hover:shadow-xl hover:to-blue-800 hover:from-green-300"}
               onClick={() => handleModalOpen(TypesOfModal.GIFTS)}
             >
-              <h2 className={`flex flex-row items-center justify-center gap-3 mb-3 text-2xl font-semibold`}>
+              <h2 className={`flex flex-row items-center justify-center gap-3 mb-3 text-2xl font-redressed`}>
                 Presentes
-                <span className="flex items-center transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                <span className="hidden sm:flex items-center transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                   <FaArrowRight />
                 </span>
               </h2>
-              <p className={`m-0 w-full text-center text-sm tracking-wide`}>
+              <p className={`hidden sm:flex justify-center m-0 w-full text-center text-sm tracking-wide`}>
                 Veja aqui como contribuir com a nossa cerimônia e nossa lua de mel!
               </p>
             </div>
--- src/components/CreatePostModal/SuspenseComponentTed.tsx
+++ src/components/CreatePostModal/SuspenseComponentTed.tsx
@@ -17,26 +17,26 @@ export default function SuspenseComponentTed() {
         <div ref={targetRef} className='grid grid-rows-2 sm:grid-cols-2 gap-y-5 sm:gap-y-none sm:gap-x-5 p-3 bg-emerald-200 w-full h-auto sm:h-80'>
             <Suspense >
                 <div className={classNameGridBox}>
-                    <p className='select-none w-2/3 sm:w-full border-t-2 border-b-2 border-solid border-emerald-500 bg-emerald-300 pl-7 font-redressed text-2xl mb-2 mt-3'>Itaú:</p>
+                    <p className='select-none flex justify-center items-center h-full sm:h-auto w-2/3 sm:w-full sm:border-y-2 sm:border-solid border-emerald-500 bg-emerald-300 sm:pl-7 font-redressed text-2xl sm:mb-2 sm:mt-3'>Itaú:</p>
 
-                    <p className='relative pl-2 font-redressed text-xl grid gap-0 grid-flow-row grid-cols-1 sm:grid-cols-2 justify-center items-center w-full sm:w-auto h-full '>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Instituição:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>Itaú Unibanco S.A. (0341)</span>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>CPF:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>134.039.467-70</span>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Nome completo:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>Lucas Oliveira Casal Teixeira</span>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Agência:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>5631</span>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>C/C:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>21609-9</span>
+                    <p className='relative sm:pl-2 font-redressed text-xl grid gap-0 grid-flow-row grid-cols-1 sm:grid-cols-2 justify-center items-center w-full sm:w-auto h-full '>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Instituição:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>Itaú Unibanco S.A. (0341)</span>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>CPF:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>134.039.467-70</span>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Nome completo:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>Lucas Oliveira Casal Teixeira</span>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Agência:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>5631</span>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>C/C:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>21609-9</span>
                     </p>
                 </div>
 
                 <div className={classNameGridBox}>
-                    <p className='select-none w-2/3 sm:w-full border-t-2 border-b-2 border-solid border-emerald-500 bg-emerald-300 pl-7 font-redressed text-2xl mb-2 mt-3'>Nubank:</p>
+                    <p className='select-none flex justify-center items-center h-full sm:h-auto w-2/3 sm:w-full sm:border-y-2 sm:border-solid border-emerald-500 bg-emerald-300 font-redressed text-2xl sm:mb-2 sm:mt-3'>Nubank:</p>
 
-                    <p className='relative pl-2 font-redressed text-xl grid gap-0 grid-flow-row grid-cols-1 sm:grid-cols-2 justify-center items-center w-full sm:w-auto  h-full '>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Instituição:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>Nu Pagamentos S.A. (0260)</span>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>CPF:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>134.039.467-70</span>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Nome completo:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>Lucas Oliveira Casal Teixeira</span>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Agência:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>0001</span>
-                        <span className='select-none bg-emerald-300 sm:bg-transparent w-full sm:w-32'>C/C:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>32130377-1</span>
+                    <p className='relative sm:pl-2 font-redressed text-xl grid gap-0 grid-flow-row grid-cols-1 sm:grid-cols-2 justify-center items-center w-full sm:w-auto  h-full '>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Instituição:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>Nu Pagamentos S.A. (0260)</span>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>CPF:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>134.039.467-70</span>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Nome completo:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>Lucas Oliveira Casal Teixeira</span>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>Agência:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>0001</span>
+                        <span className='select-none pl-2 sm:pl-0 bg-emerald-300 sm:bg-transparent w-full sm:w-32'>C/C:</span> <span className='sm:w-auto pl-7 sm:-ml-12'>32130377-1</span>
                     </p>
                 </div>
             </Suspense>
